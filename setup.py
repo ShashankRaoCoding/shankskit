81af34515a3eba29c120cdf5c9620b60a12d9df8
ShashankRao <158217706+ShashankRaoCoding@users.noreply.github.com>
2025-03-08 19:43:19 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shankskit', 
-    version='0.1.0',
+    version='0.1.1',
     author='Shashank Rao',
     author_email='your.email@example.com',
     description='A brief description of your library',
@@ -10,6 +10,7 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/ShashankRaoCoding/shankskit',
     packages=find_packages(),
+    license = "LGPL-3.0-or-later" , 
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
